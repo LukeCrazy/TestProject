5f5906a2bbbfd509d885130c083e47895d09a84e
Maple4Real <398879332@qq.com>
2019-07-23 11:42:34 +0800

    First change and update

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,4 +1,9 @@
 public class TestClass {
+
+    public void func1(){
+        System.out.println("func1");
+    }
+
     public static void main(String[] args){
         System.out.println("123456");
     }
